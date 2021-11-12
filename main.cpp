7c2e44fe021738893aa3d42a9bc7e43a34f3710d
Julien Poivret <julien@beaglebone.localdomain>
2021-11-12 22:28:25 +0100

    Better error handling an clean input mode

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <cstdlib>
 
-/*              
+/*
    - Program tested.
    - This program is a personal tool for driving with ease
-      the gpio on a beagel black from linux terminal 
+      the gpio on a beagel black from linux terminal
       ( as always perfectly suitable for iot not for time critical task. )
-     
+
      Quick help:
             compiled out name  |  0->127   | direction | on/off if out
      bash$:     programename   | pin_umber | in/out    |     1/0
@@ -17,21 +17,22 @@ struct GPIO {
     unsigned short gpio_pin;
     char direction[4];
     unsigned short value;
+    bool input;
 };
 
 int main(int argc,char* argv[])
 {
     if(argc > 1 ) {
-	/*
-	   main principe:
-	   The first argument is the gpio pin number so we need first to check 
-	   if the gpio range is valid, since we handling the arguments in char* argv[]
-	   for arithmetical range evaluation, the ascii table is so offseted down to 48 units
-	   in order to match the '0'->(value key of 48)  with  0->(valu key of 0).
-	*/
+        /*
+           main principe:
+           The first argument is the gpio pin number so we need first to check
+           if the gpio range is valid, since we handling the arguments in char* argv[]
+           for arithmetical range evaluation, the ascii table is so offseted down to 48 units
+           in order to match the '0'->(value key of 48)  with  0->(valu key of 0).
+        */
         struct GPIO data;
         size_t count = 0;
-	// if the byte is a valid numerical representation from 48 to 57 then the length of the value is so counted.
+        // if the byte is a valid numerical representation from 48 to 57 then the length of the value is so counted.
         while(argv[1][count] != 0 && count<3) {
             if (argv[1][count]>47 && argv[1][count]<58) {
                 count++;
@@ -43,26 +44,25 @@ int main(int argc,char* argv[])
         }
         data.gpio_pin = 0;
         unsigned short factor = 1;
-	// Make a big number, digit by digit... factors 1, 10, 100 fit the scale range before incrementation
-	// (1*1) + (1*10) + (1*100) = 111 , (5*1) + (3*10) + (7*100) = 735 ... easy 
+        // Make a big number, digit by digit... factors 1, 10, 100 fit the scale range before incrementation
+        // (1*1) + (1*10) + (1*100) = 111 , (5*1) + (3*10) + (7*100) = 735 ... easy
         while(count>0) {
             data.gpio_pin += ((unsigned short) argv[1][count-1] - 48) * factor;
             factor *= 10;
             count--;
         }
-	// gpio must be in a valid gpio pin range from 0 to 127 (hardware constraint).
+        // gpio must be in a valid gpio pin range from 0 to 127 (hardware constraint).
         if(data.gpio_pin>=0 && data.gpio_pin<128) {
             count = 0;
-	    // check the integrity of the second user argument.
+            // check the integrity of the second user argument.
             while(argv[2][count] != 0 && count<2 && argc >= 2) {
-                    count++;
+                count++;
             }
             if(count != 3 && count != 2) {
                 std::cout<<"The second argument is invalid must be \"in\" or \"out\""<<std::endl;
                 return EXIT_FAILURE;
             }
             bool valid = false;
-            bool input = false;
             count = 2;
             char out[4] = "out";
             char in[3] = "in";
@@ -94,7 +94,7 @@ int main(int argc,char* argv[])
                             data.direction[2] ='\0';
                             data.direction[3] ='\0';
                             valid = true;
-                            input = true;
+                            data.input = true;
                         }
                         continue;
                     }
@@ -104,11 +104,11 @@ int main(int argc,char* argv[])
                 }
             }
             if(valid) {
-                // print the data structure on screen. 
+                // print the data structure on screen.
                 std::cout<<"result: Ok -> Gpio: "<<data.gpio_pin<<", Direction: "
                          <<data.direction;
                 //Handle the third argument 1 or 0 .
-                if (!input) {
+                if (!(data.input)) {
                     if(argc<=3) {
                         std::cout<<" if gpio pin \"out\" mode then a value of 0 or 1 must be specified"<<std::endl;
                         return EXIT_FAILURE;
@@ -125,10 +125,11 @@ int main(int argc,char* argv[])
                         }
                         std::cout<<", value -> "<<data.value<<std::endl;
                     }
-                }else{
-                // if input mode.
-	        std::cout<<std::endl;
-		}
+                }
+                else {
+                    // if input mode.
+                    std::cout<<std::endl;
+                }
             }
             else {
                 std::cout<<"The second argument should be \"in\" or \"out\""<<std::endl;
@@ -139,13 +140,15 @@ int main(int argc,char* argv[])
             std::cout<<"\33[1;1H\33[2JThe GPIO number must be betwin 0 and 127"<<std::endl;
             return EXIT_FAILURE;
         }
-	// format the terminal command before the call in background (behind the data structure summary on screen). 
+        // format the terminal command before the call in background (behind the data structure summary on screen).
         // there the stack buffer a bit too hight but ok.
-	char test[100]="";
+        char test[100]="";
         std::sprintf((char*) &test, "echo %s > /sys/class/gpio/gpio%d/direction",data.direction,data.gpio_pin);
-	system(test);
-        std::sprintf((char*) &test, "echo %d > /sys/class/gpio/gpio%d/value",data.value,data.gpio_pin);
-	system(test);
+        system(test);
+        if(!data.input) {
+            std::sprintf((char*) &test, "echo %d > /sys/class/gpio/gpio%d/value",data.value,data.gpio_pin);
+            system(test);
+        }
     }
     else {
         std::cout<<"\33[1;1H\33[2JPlease enter valid arguments like \"gpio pin in/out 1/0\" \n( the \
